86ddc456660103340df2ed0501631120f718cde3
sebbbin <nsb08045@daum.net>
2024-05-19 00:07:12 +0900

    #4 [feat] 럭키 비키 메세지 get

--- hackathon/src/main/java/org/sopt/hackathon/common/dto/SuccessMessage.java
+++ hackathon/src/main/java/org/sopt/hackathon/common/dto/SuccessMessage.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
 
     POST_SUCCESS(HttpStatus.CREATED.value(), "등록이 완료되었습니다."),
+    LUCKY_ANSWER_SUCCESS(HttpStatus.OK.value(), "럭키럭키 체인지가 성공하다니 완전 럭키비키잖앙🍀"),
      ;
     private final int status;
     private final String message;
--- hackathon/src/main/java/org/sopt/hackathon/service/AnswerService.java
+++ hackathon/src/main/java/org/sopt/hackathon/service/AnswerService.java
@@ -1,9 +1,33 @@
 package org.sopt.hackathon.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.hackathon.common.dto.ErrorMessage;
+import org.sopt.hackathon.domain.Answer;
+import org.sopt.hackathon.dto.AnswerResponse;
+import org.sopt.hackathon.exception.CustomizedException;
+import org.sopt.hackathon.repository.AnswerRepository;
+import org.sopt.hackathon.repository.ConcernRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+
+    public AnswerResponse getAnswerById(Long concernId) {
+        // 걱정 ID로 답변 제공
+        Optional<Answer> optionalAnswer = answerRepository.findById(concernId);
+        if (optionalAnswer.isPresent()) {
+            Answer answer = optionalAnswer.get();
+            return new AnswerResponse(answer.getContent());
+        } else {
+            throw new CustomizedException(ErrorMessage.CONCERN_NOT_FOUND_BY_ID_EXCEPTION);
+        }
+    }
+
+
+
 }
